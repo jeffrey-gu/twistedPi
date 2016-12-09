1a95d227b60b432eca078d0bf4e4a35e96ec5eba
jgu <jeffreygu.17@gmail.com>
2016-12-08 22:42:38 -0600

    version 4.1 client .. use for scheduler

--- twisted_client/twisted_client4.py
+++ twisted_client/twisted_client4.py
@@ -1,9 +1,9 @@
 """
-now implements netstring receiver when connecting to twisted_server
+cleaned up version of twisted_client2
 """
 
 from twisted.internet.protocol import Protocol, Factory, ClientFactory
-from twisted.protocols.basic import NetstringReceiver, LineOnlyReceiver
+from twisted.protocols.basic import IntNStringReceiver, LineOnlyReceiver
 from twisted.internet import task, defer, endpoints
 import sys, socket, struct, time
 
@@ -14,41 +14,33 @@ hostName = 'bach.ese.wustl.edu'
 host = None
 defaultTwistedServerPort = 53335
 
-currTime = 0.0
 prevTime = 0.0
+currTime = 0.0
 accumTime = 0.0
 
 # use win32 reactor if applicable
 if sys.platform == 'win32':
     from twisted.internet import win32eventreactor
-
     win32eventreactor.install()
-
 
 # find hostname
 def findHost():
     addr = socket.gethostbyname(hostName)
     return addr
 
-
 ######################################################3
 
-class SocketClientProtocol(NetstringReceiver):
-    def __init__(self):
-        self.MAX_LENGTH = 33    # seven 4-byte values
+class SocketClientProtocol(LineOnlyReceiver):
+    # def __init__(self):
+        # self.delimiter = '\n'
 
     # after int prefix and other framing are removed:
-    def stringReceived(self, string):
+    def lineReceived(self, line):
         global currTime
         currTime = time.time()
-        try:
-            #format: mmm ss mm hh x y z qx qy qw qz
-            self.factory.dataBuffer.append(struct.unpack("!HBBBfffffff", string))
-            self.factory.got_msg()
-        except:
-            print 'Failed to unpack\n'
+        self.factory.got_msg(line)
 
-    def connectionMade(self):  # calls when connection is made with Twisted server
+    def connectionMade(self):   # calls when connection is made with Twisted server
         self.transport.setTcpNoDelay(True)
         print ("connected to twisted server")
         self.factory.clientReady(self)
@@ -62,7 +54,7 @@ class SocketClientFactory(ClientFactory):
 
     def __init__(self):
         self.client = None
-        self.dataBuffer = []
+        self.dataBuff = []
 
     def clientConnectionFailed(self, connector, reason):
         print ("connection failed because of %s"), reason
@@ -79,16 +71,23 @@ class SocketClientFactory(ClientFactory):
 
     def got_msg(self, msg):
         global prevTime, currTime, accumTime
+        self.dataBuff.append(msg)
         timeInterval = currTime - prevTime
 
         if prevTime > 0.0:
             accumTime += timeInterval
 
-        for payload in self.dataBuffer:
-            outputFile.write('%s\t' % str(payload))
-        outputFile.write('\tTime Interval: %f\tRunning Time: %f\n' % (timeInterval, accumTime))
+        for payload in self.dataBuff:
+            outputFile.write(payload)
+        outputFile.write('\tTime Interval: %f\tRunning Time: %f\n' %(timeInterval, accumTime))
 
+        self.dataBuff = []
         prevTime = currTime
+
+    #TODO: NOT CALLED --> do we want clients to send messages to server?
+    # def send_msg(self, msg):
+    #     if self.client:
+    #         self.client.sendLine(msg)
 
 ###########################################
 
@@ -96,8 +95,8 @@ if __name__ == '__main__':
     print('starting program')
     host = findHost()
     outputFile = open('motive_results.txt', 'w+')
-    if (host is not None):
-        print ('Attempting connection to %s:%s') % (host, defaultTwistedServerPort)
+    if(host is not None):
+        print ('Attempting connection to %s:%s') %(host, defaultTwistedServerPort)
         reactor.connectTCP(host, defaultTwistedServerPort, SocketClientFactory())
         reactor.run()
     else:
